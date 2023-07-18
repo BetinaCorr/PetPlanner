9cc380e0706474faf8118e63f5653edc4585ec3d
Betina Pinto <betina.correap@gmail.com>
2023-07-18 01:08:13 +0100

    created allergy model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_000142) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_000705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "allergies", force: :cascade do |t|
+    t.string "name"
+    t.string "comment"
+    t.bigint "pet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pet_id"], name: "index_allergies_on_pet_id"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -91,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_000142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "allergies", "pets"
   add_foreign_key "foods", "pets"
   add_foreign_key "medications", "pets"
   add_foreign_key "pets", "users"
