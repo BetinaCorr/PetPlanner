dbe58932ff40f5b4d61bcaf625e5b7c6d7e8be96
Anna Russi <annarussi@gmail.com>
2023-07-20 11:46:43 +0100

    created the new create edit update and destroy for professionals and its views

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -1,36 +1,48 @@
 class ProfessionalsController < ApplicationController
   before_action :set_pets, only: %i[index new create]
+  before_action :set_professional, only: %i[edit update destroy]
 
   def index
-    @professionals = []
-    @pet.services.each do |service|
-      @professionals << service.professional
-    end
+    @professionals = @pet.professionals
   end
 
   def new
+    @professional = Professional.new
   end
 
   def create
+    @professional = Professional.new(professional_params)
+    # Give the pet to the professional
+    @professional.pet = @pet
+    # Redirect to the Professionals page if it saves
+    if @professional.save
+      redirect_to pet_professionals_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @professional.update(professional_params)
+    redirect_to pet_professionals_path(@professional.pet)
   end
 
   def destroy
+    @professional.destroy
+    redirect_to pet_professionals_path(@professional.pet)
   end
 
   private
 
   def professional_params
-    params.require(:food).permit(:name, :category, :price, :purchase_date)
+    params.require(:professional).permit(:name, :address, :phone_number)
   end
 
   def set_professional
-    @food = Food.find(params[:id])
+    @professional = Professional.find(params[:id])
   end
 
   def set_pets
