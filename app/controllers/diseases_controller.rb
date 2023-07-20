d014f715cb9257b9b3d9aeb43a17b18f919f02d9
Betina Pinto <betina.correap@gmail.com>
2023-07-20 19:47:19 +0100

    created actions, views and routes for disease controller

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,2 +1,54 @@
 class DiseasesController < ApplicationController
+  before_action :set_pet, only: %i[index new create]
+  before_action :set_disease, only: %i[edit update destroy]
+
+  def index
+    @diseases = Disease.all
+  end
+
+  def new
+    @disease = Disease.new
+  end
+
+  def create
+    @disease = Disease.new(disease_params)
+
+    # give the pet to the disease
+    @disease.pet = @pet
+
+    # redirect to the diseases page if it saves
+    if @disease.save
+      redirect_to pet_diseases_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @disease.update(disease_params)
+    redirect_to pet_diseases_path(@disease.pet)
+  end
+
+  def destroy
+    @disease.destroy
+    redirect_to pet_diseases_path(@disease.pet)
+  end
+
+  private
+
+  def disease_params
+    params.require(:disease).permit(:name, :comment, :category)
+  end
+
+  def set_disease
+    @disease = Disease.find(params[:id])
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
+
 end
